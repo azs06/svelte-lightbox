bfbc8bbcbab3af21a794e96d3efcee97bacfbdb0
Md. Atiquzzaman Soikat <atik.aiub@gmail.com>
2025-01-27 17:46:06 +0600

    updated path

--- svelte.config.js
+++ svelte.config.js
@@ -10,7 +10,7 @@ const config = {
 			fallback: '404.html'
 		}),
 		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+			base: process.argv.includes('dev') ? '' : '/svelte-lightbox'
 		}
 	},
 	preprocess: sveltePreprocess({
